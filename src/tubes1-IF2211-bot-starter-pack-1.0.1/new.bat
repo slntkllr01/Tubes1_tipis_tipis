033f868c2a451a483efe6e35320b4cc78b7d0bed
nanthedom <naufnan@gmail.com>
2024-03-04 22:14:00 +0700

    feat: botbang.py fix oke (bismillah)

---
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/new.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic BotBang --email=brout@email.com --name=brot --password=123456 --team etimo"
+start cmd /c "python main.py --logic BotBang --email=kv@email.com --name=brototot --password=123456 --team etimo"
+start cmd /c "python main.py --logic BotBang --email=ivy@email.com --name=pret --password=123456 --team etimo"
+start cmd /c "python main.py --logic BotBang --email=vkh@email.com --name=prwot --password=123456 --team etimo"
